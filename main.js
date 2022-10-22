22e2ae55058c09124349e4d02012d81ae22559a6
mhanak96 <mateuszhanak@protonmail.com>
2022-10-22 08:28:13 +0200

    mobile version improvement 3.0

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 ['use strict']
 
 const sectionHeader = document.querySelector(".header");
+const navToogle = document.querySelector('.mobile-nav-toogle');
 
 const obs = new IntersectionObserver(function(entries){
     const entrie = entries[0];
     if(entrie.isIntersecting === false){
         document.querySelector('.primary-nav').classList.add('sticky'); 
+        navToogle.classList.add('.sticky-mobile');
     }
     if(entrie.isIntersecting === true){
         document.querySelector('.primary-nav').classList.remove('sticky'); 
+        navToogle.classList.remove('.sticky-mobile');
     }
        
     
@@ -18,3 +21,29 @@ const obs = new IntersectionObserver(function(entries){
     rootMargin: "-140px",
 });
 obs.observe(sectionHeader)
+
+//mobile toogle
+const nav = document.querySelector('.primary-nav');
+
+
+navToogle.addEventListener('click', () => {
+    const visibility = nav.getAttribute('data-visible');
+
+    if(visibility === "false"){
+        nav.setAttribute('data-visible', true);
+        navToogle.setAttribute('aria-expanded', true);
+    }
+
+    else{
+        nav.setAttribute('data-visible', false);
+        navToogle.setAttribute('aria-expanded', false);
+    }
+})
+
+//
+const nav_items = document.querySelectorAll('.primary-nav__item');
+
+nav_items.forEach(item => item.addEventListener('click', function(){
+    nav.setAttribute('data-visible', false);
+    navToogle.setAttribute('aria-expanded', false);
+}))
